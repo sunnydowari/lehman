4bf79c4466cdfdfbc5fe4feb9bb98840d4fdf618
nikunjr15 <nikunjr48@gmail.com>
2025-03-30 16:56:25 +0530

    navbar and app.jsx code update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar"; // Import Navbar
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -7,10 +7,14 @@ import Signup from "./pages/signup/Signup";
 const App = () => {
   return (
     <Router>
-      <Navbar /> {<nav>
+      <Navbar /> {/* Navbar component */}
+      
+      {/* Additional hyperlinks inside App.jsx */}
+      <nav>
         <Link to="/login">Login</Link>
         <Link to="/signup">Signup</Link>
-      </nav>}
+      </nav>
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
