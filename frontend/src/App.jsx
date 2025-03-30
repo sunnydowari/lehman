bdad911b4eda84849cf8fbeebf9ffa6ef42576e3
nikunjr15 <nikunjr48@gmail.com>
2025-03-30 16:35:00 +0530

    added navbar to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // Import Navbar
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-      </nav>
+      <Navbar /> {/* Add Navbar component here */}
 
       <Routes>
         <Route path="/login" element={<Login />} />
